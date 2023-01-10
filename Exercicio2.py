64ad4e8022b6b581b56a019e9fb50fde5197ee58
Rafael Thomaz <114960583+rafahthomaz14@users.noreply.github.com>
2023-01-09 23:37:46 -0300

    Create Exercicio2.py

---
+++ Exercicio2.py
@@ -0,0 +1,13 @@
+nome = 'Rafael Thomaz'
+altura = 1.70
+peso = 65
+imc = peso/(altura*altura) #ou peso/ altura ** altura
+
+print(nome,'tem',altura,'de altura,',)
+print('pesa',peso,'quilos e seu IMC é',)
+print(imc)
+
+# Vai mostrar:
+#Rafael Thomaz tem 1.7 de altura,
+#pesa 65 quilos e seu IMC é
+#22.49134948096886
