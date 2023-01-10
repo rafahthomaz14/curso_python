962ee5887c327581d9f1607030a67715697dbe45
Rafael Thomaz <114960583+rafahthomaz14@users.noreply.github.com>
2023-01-09 23:01:03 -0300

    Create Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,12 @@
+nome ='Rafael'
+sobrenome = 'Thomaz'
+idade = 18
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >=18
+altura_metros = 1.7
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('É maior de idade:',maior_de_idade)
+print('Altura em metros:',altura_metros)
